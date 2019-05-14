5bcaf883b460279bd4a4bae7a7a63bed37939511
Felix Steinhart <f.steinhart@fsnet.eu>
2019-05-14 13:27:55 +0200

    Add project files.

---
+++ JSInterop.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.JSInterop;
+
+namespace BlazorTest
+{
+    public class JSInterop
+    {
+        public static void ShowModalDialog(IJSRuntime jsRuntime, string dialogId)
+        {
+            jsRuntime.InvokeAsync<object>("jsFunctions.showModalDialog", dialogId);
+        }
+    }
+}
---
+++ wwwroot/js/jsFunctions.js
@@ -0,0 +1,9 @@
+﻿window.jsFunctions = {
+
+  showModalDialog: function (dialogId) {
+    const dialog = $(`#${dialogId}`);
+    console.log('dialog', dialog);
+    $(dialog).modal('show');
+  }
+
+}
